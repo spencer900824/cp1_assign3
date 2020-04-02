963227c8599a004a9a2021c814b48c16ebd4a0b1
spencer900824 <59354704+spencer900824@users.noreply.github.com>
2020-04-02 10:07:38 +0800

    Update Assign3.cpp

--- Assign3.cpp
+++ Assign3.cpp
@@ -1,19 +1,29 @@
 #include<stdio.h>
 
 int a[104];
+int ta[104][104];
+int ini = 100111;
 int len;
 int r( int s , int e , int turn)
 {
+	if( turn==0 && e%2==1)
+		return 1;
 	if( turn%2 == 0)
 	{
 		if( s == e)
 			return a[s];
  	 else
   		{
-			turn++;
-			int cs = a[s]+r( s+1 , e ,turn),ce = a[e]+r( s , e-1 ,turn);
-			return cs > ce ? cs : ce;
-
+			if( ta[s][e] == ini)
+			{
+				turn++;
+				int cs = a[s]+r( s+1 , e ,turn) , ce = a[e]+r( s , e-1 ,turn);
+				if(cs > ce)
+					ta[s][e] = cs;
+				else
+					ta[s][e] = ce;
+			}
+			return ta[s][e];
 		}
 	}
 	else
@@ -22,19 +32,28 @@ int r( int s , int e , int turn)
 			return -1*a[s];
 		else
 		{
-			turn++;
-			int cs = -1*a[s]+r( s+1 , e ,turn) , ce = -1*a[e]+r( s , e-1 ,turn);
-			return cs < ce ? cs:ce;
-
+			if( ta[e][s] == ini)
+			{
+   				turn++;
+				int cs = -1*a[s]+r( s+1 , e ,turn) , ce = -1*a[e]+r( s , e-1 ,turn);
+				if(cs < ce )
+					ta[e][s] = cs;
+				else
+				 	ta[e][s] = ce;
+			}
+			return ta[e][s];
 		}
 	}
-
 }
 
 int main()
 {
 	while( scanf("%d",&len) != -1)
 	{
+		for( int i=0 ; i<104 ; i++)
+			for( int j=0 ; j<104 ; j++)
+				ta[i][j] = ini;
+		//--------------------------------------
 		int s=0 , e=len-1;
 		for( int i=s ; i<=e ; i++)
 		{
@@ -48,6 +67,3 @@ int main()
 	}
 	return 0;
 }
-
-
-
